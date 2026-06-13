6ce6a13d556776393f79afb8b62b6ed2676cb100
hinanaseer <hinanaseer@hotmail.com>
2026-06-13 19:40:01 +0500

    feat: Initiated TechPakistan website

---
+++ main.js
@@ -0,0 +1,20 @@
+const themeBtn = document.getElementById("theme-btn");
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark-mode");
+    themeBtn.innerHTML = "☀️";
+}
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        localStorage.setItem("theme","dark");
+        themeBtn.innerHTML = "☀️";
+    }else{
+        localStorage.setItem("theme","light");
+        themeBtn.innerHTML = "🌙";
+    }
+
+});
